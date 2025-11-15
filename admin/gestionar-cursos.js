72fbbef69f99c7e472bb96854963758badaafb4b
magap875 <135629818+magap875@users.noreply.github.com>
2025-11-15 19:15:16 -0300

    avanzamos en funcionalidades y editamos estilos

--- admin/gestionar-cursos.js
+++ admin/gestionar-cursos.js
@@ -1,8 +1,20 @@
 import { crearCursos } from "../api.js";
 
 const btnAgregar = document.getElementById("agregar");
+const form = document.getElementById("formCrearCurso");
 
-btnAgregar.addEventListener("click", async () => {
+// son todos para el preview
+const previewTitulo = document.getElementById("preview-titulo");
+const previewDescripcion = document.getElementById("preview-descripcion");
+const previewDuracion = document.getElementById("preview-duracion");
+const previewCupos = document.getElementById("preview-cupos");
+const inputTitulo = document.getElementById("tituloCurso");
+const inputDescripcion = document.getElementById("descripcion");
+const inputDuracion = document.getElementById("duracion");
+const inputCupos = document.getElementById("cupos");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
 
     const titulo = document.getElementById("tituloCurso").value.trim();
     const descripcion = document.getElementById("descripcion").value.trim();
@@ -23,8 +35,9 @@ btnAgregar.addEventListener("click", async () => {
 
     try {
         await crearCursos(nuevoCurso);
-        alert("Curso creado correctamente!");
+        alert("Curso creado correctamente.");
         document.getElementById("formCrearCurso").reset();
+        actualizarPreview();
 
     } catch (error) {
         console.error(error);
@@ -32,3 +45,16 @@ btnAgregar.addEventListener("click", async () => {
     }
 });
 
+function actualizarPreview() {
+    previewTitulo.textContent = inputTitulo.value || "Título del curso.";
+    previewDescripcion.textContent = inputDescripcion.value || "Descripción del curso.";
+    previewDuracion.textContent = inputDuracion.value ? inputDuracion.value + " semanas" : "Duración.";
+    previewCupos.textContent = inputCupos.value ? inputCupos.value + " cupos" : "Cupos disponibles.";
+}
+
+// para que cada vez q escribamos en el input se actualice la card
+[inputTitulo, inputDescripcion, inputDuracion, inputCupos].forEach(input => {
+    input.addEventListener("input", actualizarPreview);
+});
+
+actualizarPreview();
